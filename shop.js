b18d18f37e568f0325792ac3ac33beadb14aea60
GenMorgan <benayoade70@gmail.com>
2023-11-24 15:14:15 +0100

    Challenge guide 2 completed; remains only a few tweaks to UI

---
+++ shop.js
@@ -0,0 +1,39 @@
+function hidehoverr(){
+    document.getElementById("hoverbody").style.display= "none";
+}
+
+
+
+
+// function startset(){
+//     let yar = document.getElementById("wrapper");
+//     let mar = document.getElementById("upp");
+//     let tar = document.getElementById("downpp");
+//   let test =  yar.classList.toggle("active")
+//     if(test){
+
+//         tar.style.display = "block"
+//         mar.style.display = "none"
+//     }
+//     else{
+        
+//         mar.style.display = "block"
+//         tar.style.display = "none"
+//     }
+
+  
+// }
+
+function startset(){
+    let yar = document.getElementById("wrapper");
+   yar.classList.toggle("active");
+   let mar = document.getElementById("downpp")
+   mar.classList.toggle("active2")
+   let tar = document.getElementById("upp")
+   tar.classList.toggle("active3")
+  
+}
+
+function expand1(){
+    
+}
